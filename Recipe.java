08e45bbbd0739bd0bd83aaa4c8b2c6aa174b5f66
przemyslaw-lacniak <79154557+przemyslaw-lacniak@users.noreply.github.com>
2021-03-03 15:17:56 +0100

    Create Recipe.java

---
+++ Recipe.java
@@ -0,0 +1,45 @@
+
+import java.util.List;
+
+public class Recipe {
+
+    private String name;
+    private int cookingTime;
+    private List<String> ingredients;
+
+    public Recipe(String name, int cookingTime, List<String> ingredients) {
+        this.name = name;
+        this.cookingTime = cookingTime;
+        this.ingredients = ingredients;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCookingTime() {
+        return cookingTime;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCookingTime(int cookingTime) {
+        this.cookingTime = cookingTime;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + ", cooking time: " + getCookingTime();
+    }
+
+}
